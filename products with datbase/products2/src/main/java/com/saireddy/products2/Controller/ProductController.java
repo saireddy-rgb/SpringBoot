28571d50a3ce2683c6499540d723c459942c3cea
saireddy-rgb <sai.reddy.shyamakura.work@gmail.com>
2024-08-23 13:10:57 +0530

    added one comment

--- products with datbase/products2/src/main/java/com/saireddy/products2/Controller/ProductController.java
+++ products with datbase/products2/src/main/java/com/saireddy/products2/Controller/ProductController.java
@@ -3,10 +3,18 @@ package com.saireddy.products2.Controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.saireddy.products2.model.Products;
 import com.saireddy.products2.service.ProductService;
 
+// this is a controller that connects to the client and service to the data
 @RestController
 public class ProductController {
 
